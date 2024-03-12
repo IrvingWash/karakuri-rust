d4b479037130e310b81fadc3d830e8acf7c836f5
Washington Irving <iamwashingyou@gmail.com>
2024-03-12 19:59:53 +0300

    fix fmt

--- karakuri/src/components.rs
+++ karakuri/src/components.rs
@@ -1,9 +1,9 @@
+pub use behavior_component::BehaviorComponent;
 pub use rigid_body_component::RigidBodyComponent;
 pub use shape_component::ShapeComponent;
 pub use transform_component::TransformComponent;
-pub use behavior_component::BehaviorComponent;
 
+mod behavior_component;
 mod rigid_body_component;
 mod shape_component;
 mod transform_component;
-mod behavior_component;
